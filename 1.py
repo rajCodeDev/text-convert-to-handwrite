8a59f6c4819a59a88bb3302fb6eab48e4e34bd40
rajCodeDev <gamedeveloper284@gmail.com>
2024-05-04 19:46:32 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,6 @@
+import pywhatkit as pw
+
+txt="""Python is easy programming . Python open your life best opportunities. Python is my favourite language"""
+
+pw.text_to_handwriting(txt,"demo1.png",[0,0,138])
+print("END")
